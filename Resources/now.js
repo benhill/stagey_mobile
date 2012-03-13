4c23faab5a74013d6a3698dc2086fe7a1bdac3be
revworks <admin@revworks.biz>
2012-03-13 00:28:33 -0700

    now playing is scrollable

--- Resources/now.js
+++ Resources/now.js
@@ -1,14 +1,29 @@
-var win = Titanium.UI.currentWindow;
 var url = "http://www.gwahir.com:3000/api/performances/7.json";
 
 var nowWin = Titanium.UI.currentWindow;
 
 var nowTab = Titanium.UI.currentTab;
 
+var nowScroll = Titanium.UI.createScrollView({
+  contentWidth:'auto',
+  contentHeight:1100,
+  top:0,
+  height:800,
+  showVerticalScrollIndicator:true,
+  showHorizontalScrollIndicator:true,
+  layout:'vertical'
+});
+
 var xhr = Ti.Network.createHTTPClient({
-  onload: function(){    
+  onload: function(){      
     var performances = JSON.parse(this.responseText).performances;
-    for (var i = 0; i < performances.length; i++) { 
+    var nowView = Ti.UI.createView({        
+      width:'auto',
+      height:2000,
+      top:0,
+      layout:'vertical'        
+    });
+    for (var i = 0; i < performances.length; i++) {       
       var performance = performances[i];
       var projectTitle = Titanium.UI.createLabel({
         text:performance.project_name,
@@ -18,7 +33,7 @@ var xhr = Ti.Network.createHTTPClient({
         left:5,
         font:{fontSize:14}      
       });   
-      win.add(projectTitle);
+      nowView.add(projectTitle);
       var projectInfo = Titanium.UI.createLabel({
         text:performance.info,
         height:'auto',
@@ -27,17 +42,16 @@ var xhr = Ti.Network.createHTTPClient({
         left:5,
         font:{fontSize:10}      
       });   
-      win.add(projectInfo);    
-
+      nowView.add(projectInfo);    
       var mapLabel = Ti.UI.createLabel({
         text: "map",
-        top:5,
+        top:0,
         left:5,
-        font:{fontSize:16},
+        font:{fontSize:12},
         height:'auto',
         width: '95%'
       });
-      win.add(mapLabel);
+      nowView.add(mapLabel);
 
       mapLabel.addEventListener('click', function(e){
         var venue_xhr = Ti.Network.createHTTPClient({
@@ -55,18 +69,18 @@ var xhr = Ti.Network.createHTTPClient({
             Ti.API.debug("STATUS: " + this.status);
             Ti.API.debug("TEXT:   " + this.responseText);
             Ti.API.debug("ERROR:  " + e.error);
-            alert('There was an error retrieving the remote data. Try again.');
           },
           timeout:5000
         });
         var venue_url = "http://www.gwahir.com:3000/api/venue/" + performance.venue_id + ".json";
-        alert(venue_url);
         venue_xhr.open("GET", venue_url);
         venue_xhr.send();  
-      });  
-
-      win.open();    
+      });              
     }
+
+    nowScroll.add(nowView);
+    nowWin.add(nowScroll);
+    nowWin.open();    
   },
   onerror: function(e) {
     Ti.API.debug("STATUS: " + this.status);
